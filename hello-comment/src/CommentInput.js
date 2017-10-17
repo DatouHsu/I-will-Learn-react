dcdbdcf0fc9f8a3ca44b038706f40826ebd2f6f1
DatouHsu <howrudatou@gmail.com>
2017-10-18 02:36:03 +0800

    Add delete comment function

--- hello-comment/src/CommentInput.js
+++ hello-comment/src/CommentInput.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class ComponentInput extends Component {
 
+  static propTypes = {
+    onSubmit: propTypes.func
+  }
+
   constructor() {
     super()
     this.state = {
@@ -22,10 +26,39 @@ class ComponentInput extends Component {
     })
   }
 
+  // private method
+  _saveUsername (username) {
+    localStorage.setItem('username', username)
+  }
+
+  handleUsernameBlur (event) {
+    this._saveUsername(event.target.value)
+  }
+
+  componentDidMount () {
+    this.textarea.focus()
+  }
+
+  //不依賴DOM操作的組件啟動的操作都可以放在componentsWillMount中進行
+  componentWillMount () {
+    this._loadUsername()
+  }
+
+  _loadUsername () {
+    const username = localStorage.getItem('username')
+    if (username) {
+      this.setState({ username })
+    }
+  }
+
   handleSubmit() {
     if (this.props.onSubmit) {
       const {username, content} = this.state
-      this.props.onSubmit({username, content})
+      this.props.onSubmit({
+        username: this.state.username,
+        content: this.state.content,
+        createdTime: +new Date()
+      })
     }
     this.setState({
       content: ''
@@ -40,6 +73,7 @@ class ComponentInput extends Component {
         <div className='comment-field-input'>
           <input 
             value = {this.state.username}
+            onBlur={this.handleUsernameBlur.bind(this)}
             onChange = {this.handleUsernameChange.bind(this)}
           />
         </div>
@@ -48,6 +82,7 @@ class ComponentInput extends Component {
         <span className='comment-field-name'>評論内容：</span>
         <div className='comment-field-input'>
           <textarea 
+            ref={(textarea) => this.textarea = textarea}
             value = {this.state.content}
             onChange = {this.handleContentChange.bind(this)}
           />
--- hello-comment/src/CommentList.js
+++ hello-comment/src/CommentList.js
@@ -3,14 +3,26 @@ import Comment from './Comment';
 
 class ComponentList extends Component {
   
+  static propTypes = {
+    comments: PropTypes.array,
+    onDeleteComment: PropTypes.func
+  }
+  
   static defaultProps = {
     comments: []
+  }
+
+  handleDeleteComment (index) {
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(index)
+    }
   }
 
   render() {
     return (
       <div>
-        {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+        {this.props.comments.map((comment, i) =>
+          <Comment comment={comment} key={i} onDeleteComment={this.handleDeleteComment.bind(this)} />)}
       </div>
     )
   }
